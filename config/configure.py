085e3e19a439114921d7b500f5cb3fbf39131ac7
sky_zhou <jqzhou_hit@hotmail.com>
2025-10-29 09:28:37 +0100

    clean working dir to import new structure

--- config/configure.py
+++
@@ -1,9 +0,0 @@
-# TODO: use proper model registration and yml instead of py file
-VALID_MODEL_NAMES = {"mask_rcnn", "google/deeplabv3_mobilenet_v2_1.0_513"}
-
-# allow segmentation class to be extended. If retrain model, server needs to be redeployed with new DEFAULT_TARGETS
-# Inference code can accept user defined targets without redeployed the server so that each client has the possibility to extend target classes
-DEFAULT_TARGETS = [8, 17]
-
-# threshold to for the binary mask, user can input his own threshold from client as well.
-DEFAULT_THRESHOLD = 0.5
